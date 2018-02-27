0ee0aac0456483ccf43b6b5efc57b734162e9331
AndresCampo <andrescampoe@gmail.com>
2018-02-26 20:23:40 -0500

    Suma terminada sin validaciones

--- app/src/main/java/com/movil/operacionesmovil2018/MainActivity.java
+++ app/src/main/java/com/movil/operacionesmovil2018/MainActivity.java
@@ -2,12 +2,31 @@ package com.movil.operacionesmovil2018;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText n1, n2;
+    private TextView res;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        n1 = findViewById(R.id.txtNumber1);
+        n2 = findViewById(R.id.txtNumber2);
+        res = findViewById(R.id.lblResult);
+
+    }
+        public void Calculate(View v){
+            double num1, num2, resultado;
+
+            num1 = Double.parseDouble(n1.getText().toString());
+            num2 = Double.parseDouble(n2.getText().toString());
+
+            resultado = Metodos.sumar(num1,num2);
+
+            res.setText(String.valueOf(resultado));
+
     }
 }
---
+++ app/src/main/java/com/movil/operacionesmovil2018/Metodos.java
@@ -0,0 +1,11 @@
+package com.movil.operacionesmovil2018;
+
+/**
+ * Created by android on 26/02/2018.
+ */
+
+public class Metodos {
+    public static double sumar(double numero1,double numero2){
+        return numero1+numero2;
+    }
+}
